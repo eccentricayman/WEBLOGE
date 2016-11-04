0639ac00ffffb5360fffe70db02ada41684daecb
lindseyma <lma2@stuy.edu>
2016-11-04 12:09:42 -0400

    barely complete app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def display():
+    return render_template("home.html")
+
+@app.route('/addStory')
+def add():
+    return render_template("add.html")
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/story')
+def myStory():
+    ##find num stories for this user on db
+    ##render template of user 
+    return render_template('story.html', username=request.form['user'])
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
