1109f1955398107c5983cf331d3ee1d31fe6d8be
eccentricayman <aahmed3@stuy.edu>
2016-11-06 01:45:14 -0400

    added utils stuff

--- utils/addStory.py
+++ utils/addStory.py
@@ -1,16 +1,27 @@
-# stories
-# name        id          updater     updates
-# ----------  ----------  ----------  ----------
+# stories table
+# name        id          summary     updater     latest     story
+# ----------  ----------  ----------  ----------  ---------  --------
 
 import sqlite3
+from flask import Flask, render_template, session
 
-def addStory(name):
+def addStory(name, summary, start):
 
     f = "data.db"
     db = sqlite3.connect(f)
     c = db.cursor()
 
     storyList = c.execute("SELECT name, id FROM stories")
+    storyID = 0
+    
     for story in storyList:
         if (name == story[0]):
-            return "Story already exists, choose a different name."
+            return render_template("addStory.html", message = "Story already exists; choose a different name.")
+        storyID = int(story[1])
+    c.execute("INSERT INTO stories VALUES ('%s', %d, '%s', %s, %s, %s)"%(name, storyID, summary, session['username'], start, start))
+
+    db.commit()
+    db.close()
+    return render_template("loadStory.html", id = storyID)
+
+
---
+++ utils/loadStory.py
@@ -0,0 +1,17 @@
+import sqlite3
+from flask import Flask, render_template, session
+
+def loadStory(storyID):
+
+    f = "data.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+    storyList = c.execute("SELECT name, id, summary, story FROM stories")
+
+    for story in storyList:
+        if (storyID == story[1]):
+            return render_template("story.html", name = story[0], summary = story[2], storyText = story[4])
+    db.commit()
+    db.close()
+    return render_template("story.html", notFound = True)
